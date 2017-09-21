f27b7c6b8dc0d4479713225ddd3e3f41de63ce23
Opla412 <florianbeaumont412@gmail.com>
2017-09-21 16:05:57 +0200

    OptinalSwift0

---
+++ cours/Optional/optional.playground/Contents.swift
@@ -0,0 +1,40 @@
+/*Optional vs Integer basic
+
+var IMAnOptional: Int? = 12             //Optional
+IMAnOptional
+
+var IMNotAnOptional: Int = 12         //Integer
+IMANotAnOptional
+ ---------------------------------------------------------------------------------------- */
+
+//Display / Unbox Optional
+
+//First method
+
+var IMAnOptional: Int? = 12
+print(IMAnOptional!)
+
+
+//Second method
+
+if IMAnOptional != nil {        //if the value is nil then the code doesn't crash
+    print(IMAnOptional!)
+}
+
+
+//Third method (most used)
+
+/* ------------------- with value testing --------------------*/
+
+if let displayAnOptional = IMAnOptional {
+    print(displayAnOptional)
+}
+
+/* ------------------ without value testing ------------------*/
+
+var IMAOptionalEmpty: Int?
+
+if let displayAnOptional = IMAOptionalEmpty {
+    print(displayAnOptional)
+}
+
